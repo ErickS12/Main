8fabf4c409058e42e6d0ef20e5e39bb27d21b11b
NieveZ <3bnievesmaribel@gmail.com>
2024-05-23 18:31:16 -0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,7 @@
+       
+public class Test {
+    public static void main(String[] args) {
+        Restaurante restaurante = new Restaurante("Restaurante Noyolo", "224577391");
+        restaurante.mostrarMenu();
+       }
+}
